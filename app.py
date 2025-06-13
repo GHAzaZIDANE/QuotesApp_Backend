fe13e989fc0a556f93e73663c54a5ec575b5eacc
Ghaza Zidane <zzidane173@gmail.com>
2025-06-13 20:14:51 +0700

    Update app.py bagian clean code

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-# Satria
 from flask import Flask, jsonify, request
 import json
 from flask_cors import CORS, cross_origin
@@ -6,27 +5,29 @@ import random
 from utils import load_quotes, filter_by_lang
 
 app = Flask(__name__)
-cors = CORS(app)
-quotes = load_quotes()
-
+CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
 @app.route("/", methods=['GET'])
 @cross_origin()
 def index():
+    """Menampilkan daftar endpoint yang tersedia."""
     return jsonify({"message": "Endpoints: /quote, /quotes"}) 
 
 @app.route('/quote', methods=['GET'])
 @cross_origin()
 def get_random_quote():
+    """Mengembalikan satu kutipan acak. Bisa difilter dengan parameter `lang`."""
     lang = request.args.get('lang')
+    quotes = load_quotes()
     filtered_quotes = filter_by_lang(quotes, lang) if lang else quotes
     return jsonify(random.choice(filtered_quotes))
 
-#Gesa
 @app.route('/quotes', methods=['GET'])
 @cross_origin()
 def get_all_quotes():
+    """Mengembalikan seluruh kutipan."""
+    quotes = load_quotes()
     return jsonify(quotes)
 
 if __name__ == '__main__':
